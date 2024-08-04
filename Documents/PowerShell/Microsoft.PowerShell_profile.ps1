5c753c09b50624f24718e22a57beec8b2fbadd30
Lukas Höppner <lukas.hoeppner@gmail.com>
2024-08-04 16:25:26 +0200

    update oh-my-posh config

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,2 @@
-oh-my-posh init pwsh --config 'C:\Program Files (x86)\oh-my-posh\themes\star.omp.json' | Invoke-Expression
+oh-my-posh init pwsh --config 'C:\Users\lukas\oh-my-posh\theme.omp.toml' | Invoke-Expression
+#oh-my-posh completion powershell | Invoke-Expression
