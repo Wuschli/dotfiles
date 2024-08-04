f9065ec479e1d6f78bbf63c1903463c37b6b65a8
Lukas Höppner <lukas.hoeppner@gmail.com>
2024-08-04 21:00:20 +0200

    omp: adjust prompt

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,2 @@
-oh-my-posh init pwsh --config 'C:\Users\lukas\oh-my-posh\theme.omp.toml' | Invoke-Expression
+oh-my-posh init pwsh --config 'C:\Users\lukas\oh-my-posh\theme.omp.yaml' | Invoke-Expression
 #oh-my-posh completion powershell | Invoke-Expression
