3fd6f8fa2d55a2e5b365c5d73eead4baad7310ab
Lukas Höppner <lukas.hoeppner@gmail.com>
2024-08-05 13:09:18 +0200

    pwsh: fix aliases

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,14 @@
 oh-my-posh init pwsh --config 'C:\Users\lukas\oh-my-posh\theme.omp.yaml' | Invoke-Expression
 #oh-my-posh completion powershell | Invoke-Expression
+
+function Do-Bat ($file){
+    bat --color=always $file
+}
+
+function Do-Ls {
+    eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions
+}
+
 Set-Alias -Name cm -Value chezmoi
-Set-Alias -Name cat -Value "bat --color=always"
-Set-Alias -Name ls -Value "eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
+Set-Alias -Name cat -Value Do-Bat
+Set-Alias -Name ls -Value Do-Ls
