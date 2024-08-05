a9099bda42640e61d7f6e86d882ba52f20f8522f
Lukas Höppner <lukas.hoeppner@gmail.com>
2024-08-05 11:24:16 +0200

    add fzf and bat

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,4 @@
 oh-my-posh init pwsh --config 'C:\Users\lukas\oh-my-posh\theme.omp.yaml' | Invoke-Expression
 #oh-my-posh completion powershell | Invoke-Expression
+Set-Alias -Name cm -Value chezmoi
+Set-Alias -Name cat -Value "bat --color=always"
