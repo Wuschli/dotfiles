d319425d4109f9c30340724b801cc81cd1c85c2a
Lukas Höppner <lukas.hoeppner@gmail.com>
2024-08-05 12:37:58 +0200

    eza: add

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -2,3 +2,4 @@ oh-my-posh init pwsh --config 'C:\Users\lukas\oh-my-posh\theme.omp.yaml' | Invok
 #oh-my-posh completion powershell | Invoke-Expression
 Set-Alias -Name cm -Value chezmoi
 Set-Alias -Name cat -Value "bat --color=always"
+Set-Alias -Name ls -Value "eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
